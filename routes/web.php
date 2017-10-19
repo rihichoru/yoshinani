dfdf36b6a5ef8fbccd294903054755d987d23a4a
Yuta Nakano <yutanakano395@icloud.com>
2017-10-20 00:21:36 +0900

    Add routes to authorization of provider

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('auth/{provider}',          'Auth\AuthController@redirectToProvider');
+Route::get('auth/{provider}/callback', 'Auth\AuthController@handleProviderCallback');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
